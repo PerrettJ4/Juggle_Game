e5cf9a79ad4c0c5882b837bdaa7bae9949f4fd9a
perrettJ4 <perrettj4@gmail.com@users.noreply.github.com>
2023-10-10 13:11:41 +0100

    canvas attempt

--- Juggle_Game/App.js
+++ Juggle_Game/App.js
@@ -1,28 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 import { GameEngine } from "react-native-game-engine";
-import Matter from 'matter-js';
-import Foot from './components/Foot';
-import Ball from './components/Ball';
+import Matter from "matter-js";
+import Foot from "./components/Foot";
+import Ball from "./components/Ball";
+
+const { SCREEN_WIDTH, SCREEN_HEIGHT } = Dimensions.get("window");
+
+import Canvas from "react-native-canvas";
 
 export default function App() {
- 
-  return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Ball/>
-      <Foot/>
-    </View>
-  );
+  const canvasRef = useRef(null);
+
+  const handleCanvas = () => {
+    const ctx = canvasRef.current.getContext("2d");
+    let raf;
+
+    const ball = {
+      x: 100,
+      y: 100,
+      vx: 5,
+      vy: 2,
+      radius: 25,
+      color: "blue",
+      draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.fillStyle = "blue";
+        ctx.fill();
+      },
+    };
+
+    function draw() {
+      ctx.clearRect(0, 0, ctx.width, ctx.height);
+      ball.draw();
+      ball.x += ball.vx;
+      ball.y += ball.vy;
+      ball.vy *= 0.99;
+      ball.vy += 0.25;
+
+      if (ball.y + ball.vy > ctx.height || ball.y + ball.vy < 0) {
+        ball.vy = -ball.vy;
+      }
+      if (ball.x + ball.vx > ctx.width || ball.x + ball.vx < 0) {
+        ball.vx = -ball.vx;
+      }
+
+      raf = window.requestAnimationFrame(draw);
+    }
+
+    raf = window.requestAnimationFrame(draw);
+
+    ball.draw();
+  };
+
+  useEffect(() => {
+    canvasRef.current.height = "100%";
+    canvasRef.current.width = "100%";
+    handleCanvas();
+  }, []);
+
+  return <Canvas style={styles.container} ref={canvasRef} />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
     borderWidth: 5,
-    borderColor: 'grey'
+    borderColor: "grey",
+    // backgroundColor: "orange",
   },
 });
---
+++ Juggle_Game/components/Game.js
@@ -0,0 +1,9 @@
+import React, { useReducer, useRef } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Animated } from "react-native";
+import { GameEngine } from "react-native-game-engine";
+import Matter from "matter-js";
+
+export default function Game() {
+  return <View></View>;
+}
